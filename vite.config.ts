cd1e1355915e50701da97fd290e6ffbace2bd90e
Markeljan Sokoli <Markeljan19@gmail.com>
2022-11-06 14:07:04 -0800

    emotion downgrade

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        commonjsOptions: {
-            transformMixedEsModules: true,
-        },
-    },
-    plugins: [react()],
-    resolve: {
-        alias: [
-            {
-                find: /^jss-plugin-(.*)$/,
-                replacement: "$1",
-                customResolver: (id) => {
-                    if (id === "{}") {
-                        id = "global";
-                    }
-                    return resolve(__dirname, `./node_modules/jss-plugin-${id}/src/index.js`);
-                },
-            },
-        ],
-    },
-});
+  plugins: [react()]
+})
