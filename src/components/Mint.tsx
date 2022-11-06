efa64f196b4c50598ac84873815f2f23188d7beb
Markeljan Sokoli <Markeljan19@gmail.com>
2022-11-05 18:15:19 -0700

    latest forntend

--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography, Button } from "@mui/material";
 import { useContext } from "react";
 import { MainContext } from "../contexts/MainContext";
+import { Image } from "mui-image";
 
 export default function MintPlayer() {
   const { NFTARENA_WRITE } = useContext(MainContext);
@@ -8,6 +9,8 @@ export default function MintPlayer() {
   return (
     <Box display="flex" flexDirection={"column"} gap={4}>
       <Typography> Mint Player </Typography>
+      <Image alt="nft-previews" src="src/assets/nft-preview.gif">
+      
       <Button onClick={() => NFTARENA_WRITE._mintPlayer()} variant="contained">
         Mint
       </Button>
