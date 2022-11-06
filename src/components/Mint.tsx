70597a5d247f964564056035453e7d52a1cfce3b
Markeljan Sokoli <Markeljan19@gmail.com>
2022-11-05 18:30:10 -0700

    image

--- src/components/Mint.tsx
+++ src/components/Mint.tsx
@@ -1,7 +1,6 @@
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, CardMedia } from "@mui/material";
 import { useContext } from "react";
 import { MainContext } from "../contexts/MainContext";
-import { Image } from "mui-image";
 
 export default function MintPlayer() {
   const { NFTARENA_WRITE } = useContext(MainContext);
@@ -9,8 +8,9 @@ export default function MintPlayer() {
   return (
     <Box display="flex" flexDirection={"column"} gap={4}>
       <Typography> Mint Player </Typography>
-      <Image alt="nft-previews" src="src/assets/nft-preview.gif">
-      
+      <CardMedia image="/static/images/cards/paella.jpg" title="Paella dish" />
+      {/* <Image alt="nft-previews" src="src/assets/nft-preview.gif"> */}
+
       <Button onClick={() => NFTARENA_WRITE._mintPlayer()} variant="contained">
         Mint
       </Button>
