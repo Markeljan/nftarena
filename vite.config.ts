731c03c02a5bf3f523341295d1800be8eaf9902a
Markeljan Sokoli <Markeljan19@gmail.com>
2022-11-06 12:47:32 -0800

    vitebuild

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true,
+        },
+    },
+    plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: /^jss-plugin-(.*)$/,
+                replacement: "$1",
+                customResolver: (id) => {
+                    if (id === "{}") {
+                        id = "global";
+                    }
+                    return resolve(__dirname, `./node_modules/jss-plugin-${id}/src/index.js`);
+                },
+            },
+        ],
+    },
+});
