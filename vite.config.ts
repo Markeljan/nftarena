14beac344fcd6afc9249283b82a87dbca5b201a7
Markeljan Sokoli <Markeljan19@gmail.com>
2022-11-06 17:09:36 -0800

    testvute

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [
+       react({
+          jsxRuntime: 'automatic', // <---
+          jsxImportSource: '@emotion/react',
+          babel: {
+            plugins: ['@emotion/babel-plugin'],
+          },
+        }),
+      ]
+  });
